5e0972a60c129eca94280a1c007dce58a0e3982d
hkk710 <hkk710@gmail.com>
2017-03-06 02:10:16 +0530

    subscriber table created

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Subscriber;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -64,12 +65,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'rollnumber' => $data['rollnumber'],
             'phonenumber' => $data['phonenumber'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+         Subscriber::create([
+            'name' => $data['name'],
+            'email'=> $data['email']
+        ]);
+        return $user;
     }
 }
