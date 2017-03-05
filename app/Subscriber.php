5f56ecc541258bc67696e32385b35b5cbe68833d
hkk710 <hkk710@gmail.com>
2017-03-06 02:21:53 +0530

    reduced code

--- app/Subscriber.php
+++ app/Subscriber.php
@@ -3,12 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class Subscriber extends Model
 {
-    use Notifiable;
 
     protected $fillable = ['name', 'email',];
 }
